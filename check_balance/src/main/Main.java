9431eb0d5cdb3cd3a069e94b9a63ec8f379c5655
Anton Ivanyutenko <anton.ivanyutenko@gmail.com>
2013-11-22 18:02:02 +0300

    Fixing bugs, need make keep-alive connection

--- check_balance/src/main/Main.java
+++ check_balance/src/main/Main.java
@@ -4,7 +4,9 @@ import java.io.BufferedReader;
 import java.io.DataOutputStream;
 import java.io.FileOutputStream;
 import java.io.IOException;
+import java.io.InputStream;
 import java.io.InputStreamReader;
+import java.io.OutputStream;
 import java.net.URL;
 import java.util.ArrayList;
 import java.util.HashMap;
@@ -16,10 +18,11 @@ import java.util.regex.Pattern;
 import javax.net.ssl.HttpsURLConnection;
 
 public class Main {
-	private static final String PASSWORD = "yK9m23sd";
 	private static final Map<String, String> HEADERS = new HashMap<String, String>();
 	private static final List<String> START_HEADERS = new ArrayList<String>();
-	private static final List<String> SIGNIN_HEADERS = new ArrayList<String>();
+	private static final List<String> CAPTCHA_HEADERS = new ArrayList<String>();
+	private static final List<String> SIGNIN_GET_HEADERS = new ArrayList<String>();
+	private static final List<String> SIGNIN_POST_HEADERS = new ArrayList<String>();
 
 	static {
 		HEADERS.put("Accept", "text/html,application/xhtml+xml,application/xml;q=0.9,image/webp,*/*;q=0.8");
@@ -37,69 +40,88 @@ public class Main {
 		START_HEADERS.add("Accept");
 		START_HEADERS.add("Accept-Encoding");
 		START_HEADERS.add("Accept-Language");
-		START_HEADERS.add("Cache-Control");
 		START_HEADERS.add("Connection");
 		START_HEADERS.add("Host");
 		START_HEADERS.add("User-Agent");
+		
+		SIGNIN_GET_HEADERS.add("Accept");
+		SIGNIN_GET_HEADERS.add("Accept-Encoding");
+		SIGNIN_GET_HEADERS.add("Accept-Language");
+		SIGNIN_GET_HEADERS.add("Connection");
+		SIGNIN_GET_HEADERS.add("Cookie");
+		SIGNIN_GET_HEADERS.add("Host");
+		SIGNIN_GET_HEADERS.add("User-Agent");
 
-		SIGNIN_HEADERS.add("Accept");
-		SIGNIN_HEADERS.add("Accept-Encoding");
-		SIGNIN_HEADERS.add("Accept-Language");
-		SIGNIN_HEADERS.add("Cache-Control");
-		SIGNIN_HEADERS.add("Connection");
-		SIGNIN_HEADERS.add("Host");
-		SIGNIN_HEADERS.add("User-Agent");
-		SIGNIN_HEADERS.add("Content-Type");
-		SIGNIN_HEADERS.add("Origin");
-		SIGNIN_HEADERS.add("Referer");
-		SIGNIN_HEADERS.add("Cookie");
+		CAPTCHA_HEADERS.add("Accept-Encoding");
+		CAPTCHA_HEADERS.add("Accept-Language");
+		CAPTCHA_HEADERS.add("Connection");
+		CAPTCHA_HEADERS.add("Host");
+		CAPTCHA_HEADERS.add("User-Agent");
+		CAPTCHA_HEADERS.add("Referer");
+		CAPTCHA_HEADERS.add("Cookie");
+		
+		SIGNIN_POST_HEADERS.add("Accept");
+		SIGNIN_POST_HEADERS.add("Accept-Encoding");
+		SIGNIN_POST_HEADERS.add("Accept-Language");
+		SIGNIN_POST_HEADERS.add("Cache-Control");
+		SIGNIN_POST_HEADERS.add("Connection");
+		SIGNIN_POST_HEADERS.add("Host");
+		SIGNIN_POST_HEADERS.add("User-Agent");
+		SIGNIN_POST_HEADERS.add("Content-Type");
+		SIGNIN_POST_HEADERS.add("Origin");
+		SIGNIN_POST_HEADERS.add("Referer");
+		SIGNIN_POST_HEADERS.add("Cookie");
 	}
 
 	/**
 	 * @param args
 	 */
 	public static void main(String[] args) {
-		String start_url = "https://ibank.belinvestbank.by/";
-		String signin_url = "URL:https://ibank.belinvestbank.by/signin";
-		String signin_url_2 = "URL:https://ibank.belinvestbank.by/signin2";
+		System.setProperty("sun.net.http.allowRestrictedHeaders", "true");
+		
+		String start_url = "https://ibank.belinvestbank.by";
+		String signin_url = "https://ibank.belinvestbank.by/signin";
 		URL url;
 		try {
 			url = new URL(start_url);
 			HttpsURLConnection con = (HttpsURLConnection) url.openConnection();
+			con.setInstanceFollowRedirects(false);
 			con.setRequestMethod("GET");
 			setHeaders(con, START_HEADERS);
-			con.connect();
+			System.out.println(con.getHeaderFields());
 
 			HEADERS.put("Cookie", con.getHeaderFields().get("Set-Cookie").get(0).split(";")[0]);
-			saveCaptcha(con);
+			
+			url = new URL(signin_url);
+			setHeaders(con, SIGNIN_GET_HEADERS);
+			System.out.println(con.getRequestProperties());
+			System.out.println(con.getHeaderFields());
+			printResponse(con);
+			System.exit(0);
+//			saveCaptcha(con);
+			
 			byte[] b = new byte[100];
 			System.in.read(b);
-			String captcha = new String(b);
+			String captcha = new String(b).trim();
 
 			url = new URL(signin_url);
 			con = (HttpsURLConnection) url.openConnection();
 			con.setRequestMethod("POST");
-			setHeaders(con, SIGNIN_HEADERS);
+			setHeaders(con, SIGNIN_POST_HEADERS);
 			StringBuilder urlParameters = new StringBuilder("login=3220389H030PB&keyword=z0q1ec&keystring=");
 			urlParameters.append(captcha);
-			con.setRequestProperty("Content-Length", Integer.toString(urlParameters.length()));
+			String paramsLength = Integer.toString(urlParameters.length());
+			con.setRequestProperty("Content-Length", paramsLength);
 			con.setDoOutput(true);
 			DataOutputStream wr = new DataOutputStream(con.getOutputStream());
 			wr.writeBytes(urlParameters.toString());
 			wr.flush();
 			wr.close();
-
-			BufferedReader in = new BufferedReader(new InputStreamReader(con.getInputStream()));
-			String inputLine;
-			StringBuffer response = new StringBuffer();
-
-			while ((inputLine = in.readLine()) != null) {
-				response.append(inputLine);
-			}
-			in.close();
-
-			// print result
-			System.out.println(response.toString());
+			
+			System.out.println(con.getResponseCode());
+			System.out.println(con.getResponseMessage());
+			System.out.println(con.getHeaderFields());
+			printResponse(con);
 		} catch (IOException e) {
 			e.printStackTrace();
 		}
@@ -120,15 +142,46 @@ public class Main {
 				response.append(inputLine);
 			}
 			in.close();
+			System.out.println(response);
 			Pattern MY_PATTERN = Pattern.compile("/captcha/\\?v=(\\d+)");
 			Matcher m = MY_PATTERN.matcher(response.toString());
 			if (m.find()) {
 				String s = m.group(1);
-				System.out.println(new StringBuilder("https://ibank.belinvestbank.by/captcha/?v=").append(s).toString());
+				String imageUrl = new StringBuilder("https://ibank.belinvestbank.by/captcha/?v=").append(s).toString();
+				URL url = new URL(imageUrl);
+				con = (HttpsURLConnection) url.openConnection();
+				setHeaders(con, CAPTCHA_HEADERS);
+				con.setRequestProperty("Accept", "image/webp,*/*;q=0.8");
+				System.out.println(con.getHeaderFields());
+				InputStream is = url.openStream();
+				OutputStream os = new FileOutputStream("captcha.jpg");
+
+				byte[] b = new byte[2048];
+				int length;
+
+				while ((length = is.read(b)) != -1) {
+					os.write(b, 0, length);
+				}
+
+				is.close();
+				os.close();
 			}
 		} catch (IOException e) {
 			e.printStackTrace();
 		}
+	}
+	
+	public static void printResponse(HttpsURLConnection con) throws IOException {
+		BufferedReader in = new BufferedReader(new InputStreamReader(con.getInputStream()));
+		String inputLine;
+		StringBuffer response = new StringBuffer();
+
+		while ((inputLine = in.readLine()) != null) {
+			response.append(inputLine);
+		}
+		in.close();
+		// print result
+		System.out.println(response.toString());
 	}
 
 }
